1f0a599379c3e263678db6b942cc58c90fc4b5e1
Ilya Zhigaras <zhigaras.dev@gmail.com>
2025-01-09 23:31:07 +0300

    add kDoc

--- adapterdelegate/src/main/java/com/zhigaras/adapterdelegate/AdapterDelegate.kt
+++ adapterdelegate/src/main/java/com/zhigaras/adapterdelegate/AdapterDelegate.kt
@@ -3,6 +3,11 @@ package com.zhigaras.adapterdelegate
 import android.view.ViewGroup
 import androidx.viewbinding.ViewBinding
 
+/**
+ * [AdapterDelegate.viewType] and [ListItem.itemType] methods should return the same for one model so check default
+ * [ListItem.itemType] method implementation first or override it in accordance with delegate [AdapterDelegate.viewType] method.
+ *
+ */
 abstract class AdapterDelegate<M : ListItem, in VH : ViewHolderDelegate<M>> {
     abstract fun viewType(): Int
     abstract fun createViewHolder(parent: ViewGroup): ViewHolderDelegate<M>
--- adapterdelegate/src/main/java/com/zhigaras/adapterdelegate/ListItem.kt
+++ adapterdelegate/src/main/java/com/zhigaras/adapterdelegate/ListItem.kt
@@ -1,5 +1,14 @@
 package com.zhigaras.adapterdelegate
 
+/**
+ * Interface for your list models. All list models must implement this and override at least one [areItemTheSame] method.
+ *
+ * **IMPORTANT**
+ *
+ * The default implementation of [areContentTheSame] method involves using data classes as list models!
+ * If you do not use data classes then override [areContentTheSame] method please.
+ *
+ */
 interface ListItem {
     
     fun itemType(): Int = this::class.hashCode()
