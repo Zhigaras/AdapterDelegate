6e23bfe77ea0eb8430e26f557319b26b1d262814
Ilya Zhigaras <zhigaras.dev@gmail.com>
2025-01-24 23:34:45 +0300

    publish setup

--- adapterdelegate/build.gradle.kts
+++ adapterdelegate/build.gradle.kts
@@ -46,8 +46,6 @@ dependencies {
     implementation(libs.androidx.recyclerview)
 }
 
-version = "1.0.0"
-
 publishing {
     mkdir("${layout.projectDirectory}\\build\\jreleaser")
     mkdir("${layout.projectDirectory}\\build\\staging-deploy")
@@ -59,41 +57,36 @@ publishing {
             afterEvaluate {
                 from(components["release"])
             }
+
             pom {
-                name.set("Adapter delegate")
-                description.set("Easy recycler view delegates and payloads")
-                url.set("https://github.com/Zhigaras/AdapterDelegate")
-                inceptionYear.set("2025")
+                name = "Adapter delegate"
+                description = "Easy recycler view delegates and payloads"
+                url = "https://github.com/Zhigaras/AdapterDelegate"
+                inceptionYear = "2025"
                 licenses {
                     license {
-                        name.set("Apache-2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        name = "Apache-2.0"
+                        url = "https://spdx.org/licenses/Apache-2.0.html"
                     }
                 }
                 developers {
                     developer {
-                        id.set("zhigaras")
-                        name.set("Zhigaras Ilya")
-                        email.set("zhigaras.dev@gmail.com")
+                        id = "zhigaras"
+                        name = "Zhigaras Ilya"
                     }
                 }
                 scm {
-                    connection.set("scm:git:https://github.com/Zhigaras/AdapterDelegate.git")
-                    developerConnection.set("scm:git:ssh://github.com/Zhigaras/AdapterDelegate.git")
-                    url.set("https://github.com/Zhigaras/AdapterDelegate")
+                    connection = "scm:git:https://github.com/zhigaras/AdapterDelegate.git"
+                    developerConnection = "scm:git:ssh://github.com/zhigaras/AdapterDelegate.git"
+                    url = "http://github.com/zhigaras/AdapterDelegate"
                 }
             }
         }
     }
+
     repositories {
         maven {
-            name = "sonatype"
-            setUrl("https://central.sonatype.com/api/v1/publisher")
-//            setUrl(layout.buildDirectory.dir("staging-deploy"))
-            credentials {
-                username = "krRhFQOP"
-                password = "+9d1JDpjVuF9uJTN3tIViU0fqJo1/rpvaPhQbv3nQ3Ev"
-            }
+            setUrl(layout.buildDirectory.dir("staging-deploy"))
         }
     }
 }
@@ -103,30 +96,104 @@ jreleaser {
         variables.set(File("${layout.projectDirectory}\\.jreleaser\\config.toml"))
     }
     gitRootSearch = true
-    project {
-        inceptionYear = "2025"
-        author("@Zhigaras")
-        maintainer("@Zhigaras")
-    }
     signing {
         active = Active.ALWAYS
         armored = true
-        verify = true
     }
     deploy {
         maven {
             mavenCentral.create("sonatype") {
                 active = Active.ALWAYS
                 url = "https://central.sonatype.com/api/v1/publisher"
-                stagingRepository(layout.buildDirectory.dir("staging-deploy").get().toString())
-                setAuthorization("Basic")
-                applyMavenCentralRules = false // Wait for fix: https://github.com/kordamp/pomchecker/issues/21
+                stagingRepository("build/staging-deploy")
+                applyMavenCentralRules = false
                 sign = true
-                checksums = true
-                sourceJar = true
-                javadocJar = true
-                retryDelay = 60
             }
         }
     }
 }
+
+version = "1.0.0"
+//
+//publishing {
+//    mkdir("${layout.projectDirectory}\\build\\jreleaser")
+//    mkdir("${layout.projectDirectory}\\build\\staging-deploy")
+//    publications {
+//        create<MavenPublication>("release") {
+//            groupId = "io.github.zhigaras"
+//            artifactId = project.name
+//
+//            afterEvaluate {
+//                from(components["release"])
+//            }
+//            pom {
+//                name.set("Adapter delegate")
+//                description.set("Easy recycler view delegates and payloads")
+//                url.set("https://github.com/Zhigaras/AdapterDelegate")
+//                inceptionYear.set("2025")
+//                licenses {
+//                    license {
+//                        name.set("Apache-2.0")
+//                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+//                    }
+//                }
+//                developers {
+//                    developer {
+//                        id.set("zhigaras")
+//                        name.set("Zhigaras Ilya")
+//                        email.set("zhigaras.dev@gmail.com")
+//                    }
+//                }
+//                scm {
+//                    connection.set("scm:git:https://github.com/Zhigaras/AdapterDelegate.git")
+//                    developerConnection.set("scm:git:ssh://github.com/Zhigaras/AdapterDelegate.git")
+//                    url.set("https://github.com/Zhigaras/AdapterDelegate")
+//                }
+//            }
+//        }
+//    }
+//    repositories {
+//        maven {
+//            name = "sonatype"
+//            setUrl("https://central.sonatype.com/api/v1/publisher")
+////            setUrl(layout.buildDirectory.dir("staging-deploy"))
+//            credentials {
+//                username = "krRhFQOP"
+//                password = "+9d1JDpjVuF9uJTN3tIViU0fqJo1/rpvaPhQbv3nQ3Ev"
+//            }
+//        }
+//    }
+//}
+//
+//jreleaser {
+//    environment {
+//        variables.set(File("${layout.projectDirectory}\\.jreleaser\\config.toml"))
+//    }
+//    gitRootSearch = true
+//    project {
+//        inceptionYear = "2025"
+//        author("@Zhigaras")
+//        maintainer("@Zhigaras")
+//    }
+//    signing {
+//        active = Active.ALWAYS
+//        armored = true
+//        verify = true
+//    }
+//    deploy {
+//        maven {
+//            mavenCentral.create("sonatype") {
+//                active = Active.ALWAYS
+//                url = "https://central.sonatype.com/api/v1/publisher"
+//                stagingRepository(layout.buildDirectory.dir("staging-deploy").get().toString())
+//                setAuthorization("Basic")
+//                applyMavenCentralRules = false // Wait for fix: https://github.com/kordamp/pomchecker/issues/21
+//                sign = true
+//                checksums = true
+//                sourceJar = true
+//                javadocJar = true
+//                retryDelay = 60
+//            }
+//        }
+//    }
+//}
