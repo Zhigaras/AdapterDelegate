9ea8f8311e86d1d0e9c42a3e896e9d9f082f925a
Ilya Zhigaras <zhigaras.dev@gmail.com>
2025-02-09 23:56:23 +0300

    add helpers

--- adapterdelegate/src/main/java/com/zhigaras/adapterdelegate/Payload.kt
+++ adapterdelegate/src/main/java/com/zhigaras/adapterdelegate/Payload.kt
@@ -2,6 +2,6 @@ package com.zhigaras.adapterdelegate
 
 import androidx.viewbinding.ViewBinding
 
-interface Payload<in VB : ViewBinding> {
+fun interface Payload<in VB : ViewBinding> {
     fun bindPayload(binding: VB)
 }
